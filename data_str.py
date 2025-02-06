75e9b7774f5ac23e5f9afd0873ad957b43088f58
sameerkumar0 <kumarsameer@seasiaconnect.com>
2025-02-06 17:54:24 +0530

    Add files via upload

---
+++ data_str.py
@@ -0,0 +1,188 @@
+list 
+
+a=[4,5,6,7,8,9,5]
+print(a)
+
+
+#length of list without len() keyword
+a=[4,5,7,5,6,8]
+count=0
+for _ in a:
+    count+=1
+print("length of list  is ",count)
+
+
+
+a=[4,5,7,5,6,8]
+print("length of list is:",len(a))
+
+a=["apple","banana","cherry","mango"]
+# for i in a:
+#     print(i,end=",")
+print(type(a))
+   
+#nested list
+mat=[
+    [1,5,5,7],
+    [8,6,3,4],
+    [5,6,2,4]
+]
+
+print(mat[1][1]) # accesing second row and second column element
+
+
+#max element present in the list
+
+a = [10, 24, 76, 23, 12]
+
+print(max(a))
+
+
+a=[10,25,45,30,7,90]
+
+largest=a[0]
+for val in a:
+    if (val > largest):
+        largest=val
+print(largest)
+
+
+a = [10, 20, 30, 40, 50, 40, 60, 40, 70]
+
+#index of 40 between index positions 4 and 8
+inde = a.index(40, 4, 8) # 4 start and 8 end parameter
+print(inde)
+
+
+
+#remove duplicate 
+a=[2,5,2,4,6,7,8,9,5,1]
+
+rem_duplicate=[]
+
+for i in a:
+    if i not in rem_duplicate:
+        rem_duplicate.append(i)
+rem_duplicate.sort()
+print(rem_duplicate)
+
+a=[2,5,2,4,6,7,8,9,5,1]
+rem_duplicate=list(set(a))
+print(rem_duplicate)
+
+
+#count the accurance of the element in the list
+
+a=[2,5,2,4,6,7,8,9,5,1]
+# print(a.count(5))
+a.sort()
+a.reverse()
+print(a)
+
+
+
+#merge two list 
+list1 = [1, 3, 5]
+list2 = [2, 4, 6]
+merge=list1+list2
+merge.sort() # sort the list 
+print(merge)
+
+num=[4,5,6,3]
+sqare=list(map(lambda x:x**2,num))
+print(sqare)
+
+
+
+## tuple----
+
+a=(2,)
+print(type(a))
+
+
+
+fruits = ("apple", "banana", "cherry","mango","kiwi")
+print(fruits)
+print(fruits[2]) # accesiing the 3rd elemenet
+
+print(fruits[0:2]) # slicing a tuple
+print("orange" in fruits)
+
+
+a=(4,6,8,4,57,4,6,4)
+print(len(a)) # len keyword is used to get the length of a tuple
+
+#comman element in both tuple
+t1 = (1, 2, 3, 4, 5)
+t2 = (4, 5, 6, 7, 8)
+
+common = tuple(set(t1) & set(t2))
+print(common) 
+
+
+#tuple packing
+name=("a","b","c")
+age=(20,22,19)
+combine=list(zip(name,age))
+print(combine)
+
+
+
+
+#dictionary
+
+d={
+    "name":"abc",
+    "age":22,
+    "phone":54216121
+}
+
+print(d)
+
+#using dict() constrauctor
+
+d1=dict(name="abs",age=21,)
+d1["gender"]="male"
+print(d1)
+print(d1.get("phone")) #get is used to ignore the error when the element is ot present
+
+for key in d1:
+    print(key)
+
+for value in d1.values():
+    print(value)
+
+
+
+#sets
+
+
+fruits = {"apple", "banana", "cherry"}
+# using set constructor
+numbers = set([1, 2, 3, 4, 4, 5])  
+
+print(fruits)    
+print(numbers)   
+
+
+#set methods 
+s={5,6,2,4}
+s.add(7) # add single value
+
+
+s.update([9,3,1])# add multiple values
+s.remove(1) # remove  value from set
+print(s)
+
+
+#set using range 
+num=set(range(1,11))
+print(num)
+
+
+a = {1, 2, 3}
+b = {1, 2, 3, 4, 5}
+
+print(a.issubset(b))  
+print(b.issuperset(a))  
+
---
+++ func.py
@@ -0,0 +1,84 @@
+# def greet(name):
+#     print (f"hello ,{name}")
+# greet("abs")
+
+
+# def add(a,b):
+#     return a+b
+# result=add(5,7)
+# print(result)
+
+
+# def fact(n):
+#     if n==0:
+#         return 1
+#     return n*fact(n-1)
+# n=int(input("enter a number :"))
+# print(fact(n))
+
+
+
+# def add_numbers(*args):
+#     return sum(args)
+
+# print(add_numbers(1, 2, 3, 4, 50))  
+
+
+
+# lambda function
+
+# add=lambda x:x+x
+# print(add(4))
+
+
+# evenOdd = lambda x: "Even" if x % 2 == 0 else "Odd"
+# x=int(input("enter number:"))
+# print(evenOdd(x))
+
+# local scope
+# def func():
+#     x=10
+#     print(x+5)
+
+# func()
+
+
+
+
+# fibonacci
+# def fibbo(n):
+#     if n <= 0:
+#         return "Invalid input"
+#     elif n == 1:
+#         return 0
+#     elif n == 2:
+#         return 1
+    
+#     a, b = 0, 1
+#     for _ in range(n - 2):
+#         a, b = b, a + b
+#     return b
+
+# n = int(input("Enter a positive number: "))
+# print(f"The {n}-th Fibonacci number is: {fibbo(n)}")
+
+# fibonacci series
+
+def fibo_series(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+n = int(input("Enter number of terms: "))
+fibo_series(n)
+
+
+# x=25  # global scope
+
+# def func():
+#     x=10 # local scope
+#     print(x)
+
+# func()
+# print(x)
