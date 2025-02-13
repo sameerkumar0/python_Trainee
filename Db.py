53a49555a7f77f041857d1a1d9826eac4b9f98a6
Sameer Kumar <kumarsameer@seasiaconnect.com>
2025-02-13 18:20:02 +0530

    changes

--- Db.py
+++ Db.py
@@ -62,10 +62,10 @@ if connection.is_connected():
     print("connection successful :")
 # create cursor object
 cursor=connection.cursor()
-## drop table (delete table )
-# dr="drop table users"
-# cursor.execute(dr)
-# connection.commit()
+# drop table (delete table )
+dr="drop table users"
+cursor.execute(dr)
+connection.commit()
 # # SQL query to create the table if it doesn't exist
 create_table_query = """
 CREATE TABLE IF NOT EXISTS users (
@@ -82,78 +82,80 @@ cursor.execute(create_table_query)
 # Commit the transaction
 connection.commit()
 
-# print("Table created ")
+print("Table created ")
 
 # ## insert data into table 
-# insert_query = "INSERT INTO users (name, age, email) VALUES (%s, %s, %s)"
+insert_query = "INSERT INTO users (name, age, email) VALUES (%s, %s, %s)"
 
 # # List of records (tuples)
-# records = [
-#     ("Alice", 28, "alice@example.com"),
-#     ("Bob", 32, "bob@example.com"),
-#     ("Charlie", 25, "charlie@example.com"),
-#     ("David", 30, "david@example.com"),
-#     ("Eve", 27, "eve@example.com")
-# ]
+records = [
+    ("Alice", 28, "alice@example.com"),
+    ("Bob", 32, "bob@example.com"),
+    ("Charlie", 25, "charlie@example.com"),
+    ("David", 30, "david@example.com"),
+    ("Eve", 27, "eve@example.com")
+]
 
 # Execute multiple inserts
-# cursor.executemany(insert_query, records) # executemany() for multiple record insertion 
-# connection.commit()
-# print("record added :")
+cursor.executemany(insert_query, records) # executemany() for multiple record insertion 
+connection.commit()
+print("record added :")
 
 
 # # read from the record
-# read="select* from users"
-# cursor.execute(read)
-# rows=cursor.fetchall()
+read="select* from users"
+cursor.execute(read)
+rows=cursor.fetchall()
 
-# for row in rows:
-#     print(row)
+for row in rows:
+    print(row)
 
 
 
 # # alter table 
-# alter="ALTER table users  add column phone varchar(20)"
-# cursor.execute(alter)
-# print("column added ")
+alter="ALTER table users  add column phone varchar(20)"
+cursor.execute(alter)
+print("column added ")
 
 # # rename using alter (Phone----> Mobile)
-# rename = "ALTER TABLE users CHANGE COLUMN phone mobile VARCHAR(20)"
-# cursor.execute(rename)
-# print("remaned successfully ")
+rename = "ALTER TABLE users CHANGE COLUMN phone mobile VARCHAR(20)"
+cursor.execute(rename)
+print("remaned successfully ")
 
 
 # # update 
-# update="UPDATE users SET age=%s WHERE name=%s"
-# data=(25,"")
-# cursor.execute(update)
+update="UPDATE users SET age=%s WHERE name=%s"
+data=(25,"")
+cursor.execute(update)
 
 
 # # delete 
-# delete="delete from users where name=%s"
-# data=("ABC",)
-# cursor.execute(delete,data)
-# print("record deleted ")
-# connection.commit()
+delete="delete from users where name=%s"
+data=("ABC",)
+cursor.execute(delete,data)
+print("record deleted ")
+connection.commit()
 
 # truncate table (remove all data from table)
-# trun="truncate table users"
-# cursor.execute(trun)
-# print("Truncate successful :")
-# connection.commit()
+trun="truncate table users"
+cursor.execute(trun)
+print("Truncate successful :")
+connection.commit()
 
-# Close the cursor and connection
+
 
 
 
 #limit() clause (retrieve geven number of record from table)
-# lim="select * from users limit %s"
-# data=(3,)
-# cursor.execute(lim,data)
-# result=cursor.fetchall()
-# for row in result:
-#     print(row)
+lim="select * from users limit %s"
+data=(3,)
+cursor.execute(lim,data)
+result=cursor.fetchall()
+for row in result:
+    print(row)
 
+
+# delete specific column
 alt="alter table users drop column mobile;"
 cursor.execute(alt)
 connection.commit()
@@ -165,6 +167,6 @@ cursor.execute(fil)
 result=cursor.fetchall()
 for row in result:
     print(row)
-
+# Close the cursor and connection
 cursor.close()
 connection.close()
